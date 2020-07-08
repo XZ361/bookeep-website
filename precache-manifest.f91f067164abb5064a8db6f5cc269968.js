6191fc627de579552208a3ed7e277b1f397f48d5
wangxiaozhan <w2457307263@outlook.com>
2020-07-08 09:17:37 +0800

    deploy

---
+++ precache-manifest.f91f067164abb5064a8db6f5cc269968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5484e6f6ca96f93fb48b",
+    "url": "/bookeep-website/css/app.651e28f7.css"
+  },
+  {
+    "revision": "b69a4286871f2de0411eaa14d3d7d358",
+    "url": "/bookeep-website/index.html"
+  },
+  {
+    "revision": "5484e6f6ca96f93fb48b",
+    "url": "/bookeep-website/js/app.c5399fd3.js"
+  },
+  {
+    "revision": "250b5fa66f7dcd4d36c6",
+    "url": "/bookeep-website/js/chunk-vendors.92ade372.js"
+  },
+  {
+    "revision": "3db71c7ab47d70475aa8134e5fc93c0e",
+    "url": "/bookeep-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookeep-website/robots.txt"
+  }
+]);
